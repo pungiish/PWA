1193f20b373c28ac3bd13efde53df8a7c94bd0d9
pungiish <jan.pungarsek@gmail.com>
2018-04-15 20:50:45 +0200

    Auto-generated commit

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,57 +1,35 @@
 var CACHE = 'cache-and-update';
 
-// On install, cache some resources.
-self.addEventListener('install', function(evt) {
-  console.log('The service worker is being installed.');
 
-  // Ask the service worker to keep installing until the returning promise
-  // resolves.
-  evt.waitUntil(precache());
+this.addEventListener('install',function(e){
+  console.log('Instaliram SW');
+
+  e.waitUntil(caches.open(CACHE).then(function(cache){
+    console.log('Precacham!')
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/favicon.ico",
+        "https://cataas.com/cat?"
+      ])
+    })
+  );
 });
 
-// On fetch, use cache but update the entry with the latest contents
-// from the server.
-self.addEventListener('fetch', function(evt) {
-  console.log('The service worker is serving the asset.');
-  // You can use `respondWith()` to answer immediately, without waiting for the
-  // network response to reach the service worker...
-  evt.respondWith(fromCache(evt.request));
-  // ...and `waitUntil()` to prevent the worker from being killed until the
-  // cache is updated.
-  evt.waitUntil(update(evt.request));
+this.addEventListener('fetch',function(e){
+  console.log('fetcham!');
+  e.respondWith(caches.match(e.request).then(function(res){
+      if(res){
+        console.log("cachano:)")
+        return res;
+      }
+      console.log("Resource ni cachan!")
+      return fetch(e.request).then(function(res){
+        console.log(res.url);
+        e.waitUntil(caches.open(CACHE).then(function(cache){
+         return cache.add(res.url)
+        }))
+        return res;
+    });
+    }));
 });
-
-// Open a cache and use `addAll()` with an array of assets to add all of them
-// to the cache. Return a promise resolving when all the assets are added.
-function precache() {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.addAll([
-      '/PWA/',
-      '/PWA/index.html',
-      '/PWA/favicon.ico',
-      '/PWA/assets/images/icons/icon-192x192.png',
-      '/PWA/\/$js/'
-    ]);
-  });
-}
-
-// Open the cache where the assets were stored and search for the requested
-// resource. Notice that in case of no matching, the promise still resolves
-// but it does with `undefined` as value.
-function fromCache(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.match(request).then(function (matching) {
-      return matching || Promise.reject('no-match');
-    });
-  });
-}
-
-// Update consists in opening the cache, performing a network request and
-// storing the new response data.
-function update(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return fetch(request).then(function (response) {
-      return cache.put(request, response);
-    });
-  });
-}
