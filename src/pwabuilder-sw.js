be8cf125ddcc1ddd4c098f50e61618685193686f
pungiish <jan.pungarsek@gmail.com>
2018-04-15 18:59:16 +0200

    fix sw

--- src/pwabuilder-sw.js
+++ src/pwabuilder-sw.js
@@ -1,48 +1,41 @@
-//This is the service worker with the Cache-first network
+var CACHE = 'cache-and-update';
 
-var CACHE = 'pwabuilder-precache';
-var precacheFiles = [
-      "./index.html",
-      "./favicon.ico",
-      "./manifest.json"
-    ];
-
-//Install stage sets up the cache-array to configure pre-cache content
+// On install, cache some resources.
 self.addEventListener('install', function(evt) {
   console.log('The service worker is being installed.');
-  evt.waitUntil(precache().then(function() {
-    console.log('[ServiceWorker] Skip waiting on install');
-      return self.skipWaiting();
 
-  })
-  );
+  // Ask the service worker to keep installing until the returning promise
+  // resolves.
+  evt.waitUntil(precache());
 });
 
-
-//allow sw to control of current page
-self.addEventListener('activate', function(event) {
-console.log('[ServiceWorker] Claiming clients for current page');
-      return self.clients.claim();
-
-});
-
+// On fetch, use cache but update the entry with the latest contents
+// from the server.
 self.addEventListener('fetch', function(evt) {
-  console.log('The service worker is serving the asset.'+ evt.request.url);
-  evt.respondWith(fromCache(evt.request).catch(fromServer(evt.request)));
+  console.log('The service worker is serving the asset.');
+  // You can use `respondWith()` to answer immediately, without waiting for the
+  // network response to reach the service worker...
+  evt.respondWith(fromCache(evt.request));
+  // ...and `waitUntil()` to prevent the worker from being killed until the
+  // cache is updated.
   evt.waitUntil(update(evt.request));
 });
 
-
+// Open a cache and use `addAll()` with an array of assets to add all of them
+// to the cache. Return a promise resolving when all the assets are added.
 function precache() {
   return caches.open(CACHE).then(function (cache) {
-    return cache.addAll(precacheFiles);
+    return cache.addAll([
+      './controlled.html',
+      './asset'
+    ]);
   });
 }
 
-
+// Open the cache where the assets were stored and search for the requested
+// resource. Notice that in case of no matching, the promise still resolves
+// but it does with `undefined` as value.
 function fromCache(request) {
-  //we pull files from the cache first thing so we can show them fast
-  console.log("FromCache");
   return caches.open(CACHE).then(function (cache) {
     return cache.match(request).then(function (matching) {
       return matching || Promise.reject('no-match');
@@ -50,18 +43,12 @@ function fromCache(request) {
   });
 }
 
-
+// Update consists in opening the cache, performing a network request and
+// storing the new response data.
 function update(request) {
-  //this is where we call the server to get the newest version of the 
-  //file to use the next time we show view
   return caches.open(CACHE).then(function (cache) {
     return fetch(request).then(function (response) {
       return cache.put(request, response);
     });
   });
-}
-
-function fromServer(request){
-  //this is the fallback if it is not in the cahche to go to the server and get it
-return fetch(request).then(function(response){ return response})
 }
